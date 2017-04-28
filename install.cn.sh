7d36e0dbcbfa611d3a3f0bb13a9eff8887364d72
izuolan <i@zuolan.me>
2017-04-29 01:30:57 +0800

    修仙中，不知道改了什么。

--- install.cn.sh
+++ install.cn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PROXY="proxychains4 -q -f ~/.dotfiles/config/proxychains4.conf"
+PROXY="proxychains4 -q -f $HOME/.dotfiles/config/proxychains4.conf"
 
 software(){
     sudo apt update && sudo apt -y dist-upgrade
@@ -8,7 +8,7 @@ software(){
 }
 
 hosts(){
-    wget https://raw.githubusercontent.com/racaljk/hosts/master/hosts -O fetchedhosts
+    wget https://coding.net/u/scaffrey/p/hosts/git/raw/master/hosts -O fetchedhosts
     mv /etc/hosts /etc/hosts-$(date +%F-%H%M%S).bak
     sed -i "s/localhost/localhost $(hostname)/g" fetchedhosts
     sed -i "s/broadcasthost/broadcasthost $(hostname)/g" fetchedhosts
@@ -25,20 +25,25 @@ proxychains4(){
     echo "Proxychains4 安装成功。"
 }
 
-ss_start(){
-    nohup sslocal -c ~/.dotfiles/config/sslocal.json >/dev/null 2>&1 &
+docker(){
+    curl -sSL https://get.docker.com/ | sh
+    echo "{"registry-mirrors": ["https://docker.mirrors.ustc.edu.cn"]}" > /etc/docker/daemon.json
 }
 
-docker(){
-    $PROXY curl -sSL https://get.docker.com/ | sh
-    echo "{"registry-mirrors": ["https://docker.mirrors.ustc.edu.cn"]}" > /etc/docker/daemon.json
+ss_start(){
+    docker run -dit --name=ss-hk \
+        -p 1080:1080 \
+        --restart=always \
+        -v ~/.dotfiles/config/sslocal.json:/etc/sslocal.json:ro \
+        easypi/shadowsocks-libev
+        -c /etc/sslocal.json
 }
 
 zsh(){
     $PROXY wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O /tmp/install.sh
     $PROXY bash /tmp/install.sh
     rm /tmp/install.sh
-    ln -sf ~/.dotfiles/config/zshrc .zshrc
+    ln -sf $HOME/.dotfiles/config/zshrc .zshrc
 }
 
 vim(){
@@ -48,7 +53,7 @@ vim(){
 }
 
 tmux(){
-    cd ~/ && rm -rf ~/.tmux
+    cd $HOME && rm -rf $HOME/.tmux
     $PROXY git clone https://github.com/gpakosz/.tmux.git
     ln -sf .tmux/.tmux.conf
     cp .tmux/.tmux.conf.local .
@@ -56,18 +61,18 @@ tmux(){
 
 aria2_config(){
     echo -n "Please enter the Aria2 remote password (can be downloaded remote monitoring progress):"; read -s PASS;echo ""
-    sed -i "s:ARIA2_PASSWORD:$2:g" ~/.dotfiles/config/aria2.conf
+    sed -i "s:ARIA2_PASSWORD:$2:g" $HOME/.dotfiles/config/aria2.conf
     echo "successfully."
 }
 ss_config(){
     echo "现在配置 Shadowsocks 代理。"
     echo -n "请输入 Shadowsocks 账号的 IP:"; read SS_SERVER_IP;echo ""
-    sed -i "s|"server": ""|"server": "$SS_SERVER_IP"|g" ~/.dotfiles/config/sslocal.json
+    sed -i "s|"server": ""|"server": "$SS_SERVER_IP"|g" $HOME/.dotfiles/config/sslocal.json
     echo -n "请输入 Shadowsocks 账号的端口:"; read SS_SERVER_PORT;echo ""
-    sed -i "s|"server_port": ""|"server_port": "$SS_SERVER_PORT"|g" ~/.dotfiles/config/sslocal.json
+    sed -i "s|"server_port": ""|"server_port": "$SS_SERVER_PORT"|g" $HOME/.dotfiles/config/sslocal.json
     echo -n "请输入 Shadowsocks 账号的密码（输入不可见）:"; read -s SS_SERVER_PASSWORD;echo ""
-    sed -i "s|"password": ""|"password": "$SS_SERVER_PASSWORD"|g" ~/.dotfiles/config/sslocal.json
-    echo "其他情况请自行编辑 ~/.dotfiles/config/sslocal.json 这个文件以配置代理。"
+    sed -i "s|"password": ""|"password": "$SS_SERVER_PASSWORD"|g" $HOME/.dotfiles/config/sslocal.json
+    echo "其他情况请自行编辑 $HOME/.dotfiles/config/sslocal.json 这个文件以配置代理。"
 }
 while getopts ":p:sh" optname
   do
@@ -80,18 +85,18 @@ while getopts ":p:sh" optname
     esac
   done
 
-if [ ! -f ~/.dotfiles ]; then 
-    git clone https://github.com/izuolan/dotfiles.git ~/.dotfiles
+if [ ! -f $HOME/.dotfiles ]; then 
+    git clone https://github.com/izuolan/dotfiles.git $HOME/.dotfiles
 else
     echo ".dotfiles 已经存在，更新脚本。"
-    cd ~/.dotfiles && git pull
+    cd $HOME/.dotfiles && git pull
 fi
-chmod a+x ~/.dotfiles/script/*
+chmod a+x $HOME/.dotfiles/script/*
 software
 hosts
 proxychains4
-ss_start
 docker
+ss_start
 zsh
 source .zshrc
 tmux
